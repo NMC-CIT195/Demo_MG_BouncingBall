2dd517635a1ec018f9744a15e9b1a33ce69cbaf0
John Velis <john@johnvelis.com>
2015-10-31 14:05:12 -0400

    commit205

--- Demo_MG_ClickBall.D00/ClickBall.cs
+++ Demo_MG_ClickBall.D00/ClickBall.cs
@@ -5,27 +5,21 @@ using Microsoft.Xna.Framework.Input;
 namespace Demo_MG_ClickBall
 {
     /// <summary>
-    /// initial layout of game including walls and a ball
+    /// This is the main type for your game.
     /// </summary>
     public class ClickBall : Game
     {
-        // declare instance variables for the sprites
-        private string _ballSpriteName;
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+
+        // declare instance variables for the ball sprite and vector position
+        private Texture2D _ball;
         private Vector2 _ballPosition;
-        private Ball _ball;
-       
 
-        // declare a spriteBatch object
-        private SpriteBatch _spriteBatch;
-
-        private GraphicsDeviceManager _graphics;
-
-        /// <summary>
-        /// constructor initializes the game objects
-        /// </summary>
         public ClickBall()
         {
             _graphics = new GraphicsDeviceManager(this);
+
 
             // set the window size 
             _graphics.PreferredBackBufferWidth = 640;
@@ -35,7 +29,7 @@ namespace Demo_MG_ClickBall
         }
 
         /// <summary>
-        /// method allows the game to perform any initialization it needs to before starting to run.
+        /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
         /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
@@ -46,73 +40,60 @@ namespace Demo_MG_ClickBall
             _ballPosition.X = 100;
             _ballPosition.Y = 200;
 
-            _ballSpriteName = "Ball";
-
-
             base.Initialize();
         }
 
         /// <summary>
-        /// method will be called once per game and is the place to load
+        /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
         protected override void LoadContent()
         {
+            // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            _ball = new Ball(Content, _ballSpriteName, _ballPosition);
-
-            _ball.Draw(_spriteBatch);
+            _ball = Content.Load<Texture2D>("ball");
         }
 
         /// <summary>
-        /// method will be called once per game and is the place to unload
+        /// UnloadContent will be called once per game and is the place to unload
         /// game-specific content.
         /// </summary>
         protected override void UnloadContent()
         {
-            // Unload any non ContentManager content here
+            // TODO: Unload any non ContentManager content here
         }
 
         /// <summary>
-        /// method allowing the game to run logic such as updating the world,
+        /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // detect an Escape key press to end the game
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-            {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            }
 
-            _ball.Active = true;
+            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
 
         /// <summary>
-        /// method to add all of the current sprites to the next game screen
+        /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            
+
             _spriteBatch.Begin();
 
-            _ball.Draw(_spriteBatch);
+            _spriteBatch.Draw(_ball, _ballPosition, Color.White);
 
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
-
-        #region HELPER METHODS
-
-
-
-        #endregion
     }
 }

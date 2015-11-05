1c4151bdcf036b1c2e3c873e502a996cda148807
John Velis <john@johnvelis.com>
2015-11-05 15:59:07 -0500

    commit Demo D05

---
+++ Demo_MG_ClickBall.D05/Ball.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Demo_MG_ClickBall
+{
+    public class Ball
+    {
+
+        #region FIELDS
+
+        private ContentManager _contentManager;
+        private string _spriteName;
+        private Texture2D _sprite;
+        private int _spriteSize;
+        private Vector2 _position;
+        private Vector2 _velocity;
+        private bool _active;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public ContentManager ContentManager
+        {
+            get { return _contentManager; }
+            set { _contentManager = value; }
+        }
+
+        public string SpriteName
+        {
+            get { return _spriteName; }
+            set { _spriteName = value; }
+        }
+
+        public Texture2D Sprite
+        {
+            get { return _sprite; }
+            set { _sprite = value; }
+        }
+
+        public int SpriteSize
+        {
+            get { return _spriteSize; }
+            set { _spriteSize = value; }
+        }
+
+        public Vector2 Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public Vector2 Velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+
+        public bool Active
+        {
+            get { return _active; }
+            set { _active = value; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// instantiate a new ball
+        /// </summary>
+        /// <param name="contentManager">game content manager object</param>
+        /// <param name="spriteName">file name of sprite</param>
+        /// <param name="positiion">vector position of ball</param>
+        public Ball(
+            ContentManager contentManager,
+            string spriteName,
+            int spriteSize,
+            Vector2 positiion,
+            Vector2 velocity
+            )
+        {
+            _contentManager = contentManager;
+            _spriteName = spriteName;
+            _spriteSize = spriteSize;
+            _position = positiion;
+            _velocity = velocity;
+
+            // load the ball image into the Texture2D for the ball sprite
+            _sprite = _contentManager.Load<Texture2D>(_spriteName);
+        }
+
+        #endregion
+
+        #region METHODS
+        /// <summary>
+        /// add ball sprite to the SpriteBatch object
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // only draw the ball if it is active
+            if (_active)
+            {
+                spriteBatch.Draw(_sprite, _position, Color.White);
+            }
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ Demo_MG_ClickBall.D05/ClickBall.cs
@@ -0,0 +1,358 @@
+﻿using System; // add to allow Windows message box
+using System.Runtime.InteropServices; // add to allow Windows message box
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Demo_MG_ClickBall
+{
+    /// <summary>
+    /// initial layout of game including walls and a ball
+    /// </summary>
+    public class ClickBall : Game
+    {
+        // add code to allow Windows message boxes when running in a Windows environment
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern uint MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        // set the cell size in pixels
+        private const int CELL_WIDTH = 64;
+        private const int CELL_HEIGHT = 64;
+
+        // set the map size in cells
+        private const int MAP_CELL_ROW_COUNT = 8;
+        private const int MAP_CELL_COLUMN_COUNT = 10;
+
+        // set the window size
+        private const int WINDOW_WIDTH = MAP_CELL_COLUMN_COUNT * CELL_WIDTH;
+        private const int WINDOW_HEIGHT = MAP_CELL_ROW_COUNT * CELL_HEIGHT;
+
+        // set the location for the score
+        private const int SCORE_X_POSTION = 500;
+        private const int SCORE_Y_POSTION = 20;
+
+        // set the winning score
+        private const int WINNING_SCORE = 5;
+
+        // set the time limit seconds
+        private const double TIME_LIMIT = 5;
+
+        // create a random number set
+        private Random _randomNumbers = new Random();
+
+        // declare instance variables for the background
+        private Texture2D _background;
+        private Rectangle _backgroundPosition;
+
+        // declare instance variables for the sprites
+        private List<Ball> _balls;
+
+        // declare a spriteBatch object
+        private SpriteBatch _spriteBatch;
+
+        // declare a MouseState object to get mouse information
+        private MouseState _mouseOldState;
+        private MouseState _mouseNewState;
+
+        // declare a SpriteFont for the on-screen score
+        private SpriteFont _scoreFont;
+
+        // declare a variable to store the score
+        private int _score;
+
+        // declare a variable for the timer
+        private double _timer = TIME_LIMIT;
+
+        // declare a SoundEffect for the explosion
+        private SoundEffect _explosion;
+
+        private GraphicsDeviceManager _graphics;
+
+        /// <summary>
+        /// constructor initializes the game objects
+        /// </summary>
+        public ClickBall()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+
+            // set the window size 
+            _graphics.PreferredBackBufferWidth = MAP_CELL_COLUMN_COUNT * CELL_WIDTH;
+            _graphics.PreferredBackBufferHeight = MAP_CELL_ROW_COUNT * CELL_HEIGHT;
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// method allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // set the background's initial position
+            _backgroundPosition = new Rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+            // create a ball object
+            _balls = new List<Ball>();
+            Ball ball01 = new Ball(Content, "Ball", 64, new Vector2(300, 200), new Vector2(2, 2));
+            _balls.Add(ball01);
+            Ball ball02 = new Ball(Content, "Ball", 64, new Vector2(100, 400), new Vector2(2, 2));
+            _balls.Add(ball02);
+            Ball ball03 = new Ball(Content, "small_ball", 32, new Vector2(100, 400), new Vector2(4, -4));
+            _balls.Add(ball03);
+
+            // make the balls active
+            ball01.Active = true;
+            ball02.Active = true;
+            ball03.Active = true;
+
+            // make mouse visible on game
+            this.IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// method will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // load the background sprite
+            // ball sprite loaded when instantiated
+            _background = Content.Load<Texture2D>("BackgroundSandyStained");
+
+            // load the font for the score
+            _scoreFont = Content.Load<SpriteFont>("ScoreFont");
+
+            // load the explosion
+            _explosion = Content.Load<SoundEffect>("Explosion01");
+        }
+
+        /// <summary>
+        /// method will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// main game loop
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // player still playing
+            if ((_score != WINNING_SCORE) && (_timer > 0))
+            {
+                HandleKeyboardEvents();
+                HandleMouseEvents();
+                UpdateBallMovement(_balls);
+                UpdateTimer(gameTime);
+
+                base.Update(gameTime);
+            }
+            // player wins
+            else if (_score == WINNING_SCORE)
+            {
+                DisplayWinScreen();
+            }
+            // player out of time
+            else if (_timer <= 0)
+            {
+                DisplayOutOfTimeMessage();
+            }
+
+        }
+
+        /// <summary>
+        /// method to add all of the current sprites to the next game screen
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            // draw the background and the ball
+            _spriteBatch.Draw(_background, _backgroundPosition, Color.White);
+            foreach (var ball in _balls)
+            {
+                ball.Draw(_spriteBatch);
+            }
+
+
+            DrawScoreTimer();
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        #region HELPER METHODS
+
+
+        /// <summary>
+        /// method to move the ball forward and bounce off the sites of the screen
+        /// </summary>
+        /// <param name="_ball">ball object</param>
+        private void UpdateBallMovement(List<Ball> balls)
+        {
+            foreach (var ball in balls)
+            {
+                if (ball.Active)
+                {
+                    BounceOffWalls(ball);
+                    ball.Position += ball.Velocity;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// method to bounce ball of walls
+        /// </summary>
+        public void BounceOffWalls(Ball ball)
+        {
+            // ball is at the top or bottom of the window, change the Y direction
+            if ((ball.Position.Y > WINDOW_HEIGHT - ball.SpriteSize) || (ball.Position.Y < 0))
+            {
+                ball.Velocity = new Vector2(ball.Velocity.X, -ball.Velocity.Y);
+            }
+            // ball is at the left or right of the window, change the X direction
+            else if ((ball.Position.X > WINDOW_WIDTH - ball.SpriteSize) || (ball.Position.X < 0))
+            {
+                ball.Velocity = new Vector2(-ball.Velocity.X, ball.Velocity.Y);
+            }
+        }
+
+        /// <summary>
+        /// method to catch and handle mouse events
+        /// </summary>
+        private void HandleMouseEvents()
+        {
+            // get the current state of the mouse
+            _mouseNewState = Mouse.GetState();
+
+            // left mouse button was a click
+            if (_mouseNewState.LeftButton == ButtonState.Pressed && _mouseOldState.LeftButton == ButtonState.Released)
+            {
+                // check all balls
+                foreach (var ball in _balls)
+                {
+                    // if the mouse is over the ball and left button is clicked, destroy and spawn the ball
+                    if (MouseOnBall(ball))
+                    {
+                        _explosion.CreateInstance().Play();
+                        Spawn(ball);
+                        _score++;
+                    }
+                }
+            }
+
+            // store the current state of the mouse as the old state
+            _mouseOldState = _mouseNewState;
+        }
+
+        /// <summary>
+        /// method to determine if the mouse is on the ball
+        /// </summary>
+        /// <returns></returns>
+        private bool MouseOnBall(Ball ball)
+        {
+            bool mouseClickedOnBall = false;
+
+            // get the current state of the mouse
+            MouseState mouseState = Mouse.GetState();
+
+            // mouse over ball
+            if ((_mouseNewState.X > ball.Position.X) &&
+                (_mouseNewState.X < (ball.Position.X + 64)) &&
+                (_mouseNewState.Y > ball.Position.Y) &&
+                (_mouseNewState.Y < (ball.Position.Y + 64)))
+            {
+                mouseClickedOnBall = true;
+            }
+
+            return mouseClickedOnBall;
+        }
+
+        /// <summary>
+        /// spawn the ball at a random location on the screen
+        /// </summary>
+        /// <param name="ball">ball object</param>
+        private void Spawn(Ball ball)
+        {
+            // find a valid location to spawn the ball
+            int ballXPosition = _randomNumbers.Next(WINDOW_WIDTH - CELL_WIDTH);
+            int ballYPosition = _randomNumbers.Next(WINDOW_HEIGHT - CELL_HEIGHT);
+
+            // set ball's new position and reverse direction
+            ball.Position = new Vector2(ballXPosition, ballYPosition);
+            ball.Velocity = -ball.Velocity;
+        }
+
+        /// <summary>
+        /// method to catch and handle keyboard events
+        /// </summary>
+        private void HandleKeyboardEvents()
+        {
+            // detect an Escape key press to end the game
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                // demonstrate the use of a Window's message box to display information
+                MessageBox(new IntPtr(0), "Escape key pressed Click OK to exit.", "Debug Message", 0);
+                Exit();
+            }
+        }
+
+        /// <summary>
+        /// method to draw the current score and the timer on the game screen
+        /// </summary>
+        private void DrawScoreTimer()
+        {
+            _spriteBatch.DrawString(_scoreFont, "Score: " + _score, new Vector2(SCORE_X_POSTION, SCORE_Y_POSTION), Color.Black);
+            _spriteBatch.DrawString(_scoreFont, "Time: " + _timer.ToString("000"), new Vector2(SCORE_X_POSTION, SCORE_Y_POSTION + 25), Color.Black);
+        }
+
+        /// <summary>
+        /// method to display the Win screen
+        /// </summary>
+        private void DisplayWinScreen()
+        {
+            MessageBox(new IntPtr(0), "You have won the game!\n Press any key to exit.", "Debug Message", 0);
+            Exit();
+        }
+
+        /// <summary>
+        /// method to up the game timer in seconds
+        /// </summary>
+        /// <param name="gameTime"></param>
+        private void UpdateTimer(GameTime gameTime)
+        {
+            _timer -= gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        /// <summary>
+        /// method to display an out of time message
+        /// </summary>
+        private void DisplayOutOfTimeMessage()
+        {
+            MessageBox(new IntPtr(0), "Sorry, you ran out of time.\n Press any key to exit.", "Debug Message", 0);
+            Exit();
+        }
+
+        #endregion
+
+
+    }
+}
